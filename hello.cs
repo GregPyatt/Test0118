94f929f9bfbb6aea99cac3a4a80d8cdfc96712dd
Greg Pyatt <greg.pyatt@gmail.com>
2018-02-24 01:32:17 -0800

    initialize repository and first commit

---
+++ hello.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Test0118
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello from Linux DotNet Core");
+        }
+    }
+}
